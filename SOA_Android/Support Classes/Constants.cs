0208a2f144733203fd9440989b8b0942f30e44dd
Shiigu <cristi1991_10@hotmail.com>
2016-11-19 01:58:51 -0300

    Updating Dweet URLs

--- SOA_Android/Support Classes/Constants.cs
+++ SOA_Android/Support Classes/Constants.cs
@@ -4,8 +4,8 @@ namespace SOA_Android.Support_Classes
 {
     public static class Constants
     {
-        public const string DweetPostHttp = "https://dweet.io/dweet/for/soa-ici-sensor";
+        public const string DweetPostHttp = "https://dweet.io/dweet/for/soa-ici-android";
         public const string DweetPostColorsHttp = "https://dweet.io/dweet/for/soa-ici-colors";
-        public const string DweetGetHttp = "https://dweet.io/get/latest/dweet/for/soa-ici-sensor";
+        public const string DweetGetHttp = "https://dweet.io/get/latest/dweet/for/soa-ici-arduino";
     }
 }
